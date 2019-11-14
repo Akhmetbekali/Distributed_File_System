2fb380c003365e03d5a193c341d857b2b1b79f80
akhmetbekali <akhmetbek.ali@gmail.com>
2019-11-14 22:43:52 +0300

    Refactored client

--- server.py
+++ server.py
@@ -64,7 +64,6 @@ def client_server():
                 conn.send(pickle.dumps(storage_address))
                 # storage_address = "IP: 192.168.0.136 \n Port: 8000"  # IP of Data storage
                 storage_server(data, path)
-
         elif data.split()[0] == "Read" and data.split()[1] == "file":
             filename = data.split()[-1]
             fileread(conn, filename)
@@ -268,20 +267,17 @@ def storage_server(message, path):
     if data == "Clear":
         print("OK")
         return "OK"
-    elif data == "Ready to Upload" or "Ready to Download":
-        client_socket.send(pickle.dumps(path))
-    else:
+    elif data == "error":
         print("Error")
         return data
-    # elif data == "Ready to" + message:
-        # with open('test.txt', 'rb') as fh:
-        #     fh.seek(0, 0)
-        #     last_two = fh.readlines()[-2:]
-        #     last = last_two[1].decode().split('] ')[1]
-        #     path = last_two[0].decode().split('] ')[1].split(' ')[1]
-        # if last == "FTP session closed (disconnect).":
-        #     kill = ''  # Kill process of client_storage in storage.py
-        # print("Ready to " + message)
+    elif data == "Ready to" + message:
+        with open('test.txt', 'rb') as fh:
+            fh.seek(0, 0)
+            last_two = fh.readlines()[-2:]
+            last = last_two[1].decode().split('] ')[1]
+            path = last_two[0].decode().split('] ')[1].split(' ')[1]
+        if last == "FTP session closed (disconnect).":
+            kill = ''  # Kill process of client_storage in storage.py
     client_socket.close()
 
 
@@ -299,5 +295,5 @@ def ping(connection):
 if __name__ == '__main__':
     messages = ["Initialize", "Create file", "Delete file",
                 "File info", "Copy file", "Move file", "Open directory", "Read directory",
-                "Make directory", "Delete directory", "Upload", "Download"]
+                "Make directory", "Delete directory"]
     client_server()
