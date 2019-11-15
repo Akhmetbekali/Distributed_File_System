371717af1e62fd7bab94aff76eb9ba30a6ce8c51
akhmetbekali <akhmetbek.ali@gmail.com>
2019-11-16 00:20:33 +0300

    Edited

--- server.py
+++ server.py
@@ -3,14 +3,15 @@ import pickle
 import socket
 import shutil
 
-ds1_ip = "3.15.172.241"
+ds1_ip = "192.168.1.57"
 # ds2_ip = "18.221.170.198"
 client_ip = "192.168.1.52"
-ns_ip = "192.168.1.57"
+ns_ip = "3.15.172.241"
 ftp_port = 20
 ns_client_port = 8081
 ns_ds_port = 8080
 ds_ds_tcp_port = 8082
+
 
 def client_server():
 
