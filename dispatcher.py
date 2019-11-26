512e6e14243a352744ec80c64ab6e4bcd31e2921
Dapimex <denis-pimenov2011@yandex.ru>
2019-11-26 04:24:10 +0300

    Server update

--- dispatcher.py
+++ dispatcher.py
@@ -418,7 +418,7 @@ def listen_newcomer_ds():
             else:
                 file_containers.append(address[0])
             for file in path_map.values():
-                print(file[0] + " " + hashcode)
+                print(str(file[0] == hashcode))
                 if file[0] == str(hashcode):
                     if file[1] is not None:
                         file[1] = file_info
